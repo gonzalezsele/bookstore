1b768abdfb8cc11aa960341d9ff98aec51d4d592
Selene González Blanco <sgonzalez2803@gmail.com>
2023-08-05 19:30:38 +0200

    Init bookstore ecommerce

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const overlay = document.querySelector("[data-overlay]");
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [overlay, navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+  });
+}
+
+/**
+ * add active class on header when scrolled 200px from top
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 200 ? header.classList.add("active")
+    : header.classList.remove("active");
+})
+
+
+  var currentImg = 0;
+  var imgs = document.querySelectorAll('.slider img');
+  let dots = document.querySelectorAll('.dot');
+  var interval = 3000;
+
+  // Second banner
+  var secondEventTitle = 'Hi! *Freshmen* Orientation Day';
+
+  // Third banner
+  var thirdEventDate = new Date('2023-02-01'); // pull this from database
+  var thirdEventDateString = thirdEventDate.toLocaleDateString('en-us', { year: 'numeric', month: 'short', day: 'numeric' });
+  var days = calculateDays(new Date(), thirdEventDate) || 0;
+  const countdownText = days > 0 ? `${days} days left` : 'Live Now!';
+
+  var secondImageUrl = `https://ondemand.bannerbear.com/simpleurl/01YWAxB7nGYdJrKoXM/title/text/${encodeURIComponent(secondEventTitle)}`;
+  var thirdImageUrl = `https://ondemand.bannerbear.com/simpleurl/ley9O0B2ZGbB4GjRDY/date/text/${encodeURIComponent(
+    thirdEventDateString
+  )}/countdown/text/${encodeURIComponent(countdownText)}`;
+
+  document.getElementById('img-2').src = secondImageUrl;
+  document.getElementById('img-3').src = thirdImageUrl;
+
+  var timer = setInterval(changeSlide, interval);
+
+  function changeSlide(n) {
+    for (var i = 0; i < imgs.length; i++) {
+      imgs[i].style.opacity = 0;
+      dots[i].className = dots[i].className.replace(' active', '');
+    }
+
+    currentImg = (currentImg + 1) % imgs.length;
+
+    if (n != undefined) {
+      clearInterval(timer);
+      timer = setInterval(changeSlide, interval);
+      currentImg = n;
+    }
+
+    imgs[currentImg].style.opacity = 1;
+    dots[currentImg].className += ' active';
+  }
+
+  function calculateDays(today, eventDate) {
+    const difference = eventDate.getTime() - today.getTime();
+
+    return Math.ceil(difference / (1000 * 3600 * 24)); // convert to days
+  }
